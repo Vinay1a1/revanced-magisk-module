8035c015f2be6432c21ca940b184ccb914db9d2a
Vinay1a1 <109431786+Vinay1a1@users.noreply.github.com>
2024-10-16 21:04:25 +0530

    Update utils.sh

--- utils.sh
+++ utils.sh
@@ -408,7 +408,7 @@ get_archive_pkg_name() { echo "$__ARCHIVE_PKG_NAME__"; }
 patch_apk() {
 	local stock_input=$1 patched_apk=$2 patcher_args=$3 rv_cli_jar=$4 rv_patches_jar=$5
 	local cmd="java -jar $rv_cli_jar patch $stock_input -p -o $patched_apk -b $rv_patches_jar  $patcher_args --keystore=ks.keystore \
---keystore-entry-password=${KEYSTORE_PASS} --keystore-password=${KEYSTORE_PASS} --signer=jhc --keystore-entry-alias=jhc --options=options.json"
+--keystore-entry-password=${KEYSTORE_PASS} --keystore-password=${KEYSTORE_PASS} --signer=V --keystore-entry-alias=ks --options=options.json"
 	if [ "$OS" = Android ]; then cmd+=" --custom-aapt2-binary=${AAPT2}"; fi
 	pr "$cmd"
 	if eval "$cmd"; then [ -f "$patched_apk" ]; else
